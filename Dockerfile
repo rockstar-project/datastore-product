2d284554176f86ad5bd2708d5449112fe9fbdb1a
Sal Siddiqui <salsiddiqui0@gmail.com>
2018-02-12 04:16:06 -0600

    updated

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,4 @@
 FROM mysql
 COPY schema.sql /docker-entrypoint-initdb.d/data-dump.sql
+WORKDIR /app/data
+COPY data .
--- script.sh
+++ script.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-ACCESS_TOKEN=$(curl --silent -X POST -H 'Accept: application/json' -H 'Content-Type: application/json; charset=UTF-8' -d '{"username":"'"$ADMIN_USER"'", "password":"'"$ADMIN_PASSWORD"'", "client_id":"'"$ADMIN_CLIENT_ID"'", "client_secret":"'"$ADMIN_CLIENT_SECRET"'", "audience":"https://api.rock-star.io/", "grant_type":"password" }' https://rockstar.auth0.com/oauth/token | jq -r .access_token)
+ACCESS_TOKEN=$(curl --silent -X POST -H 'Accept: application/json' -H 'Content-Type: application/json; charset=UTF-8' -d '{"username":"'"$USER"'", "password":"'"$PASSWORD"'", "client_id":"'"$CLIENT_ID"'", "client_secret":"'"$CLIENT_SECRET"'", "audience":"https://api.rock-star.io/", "grant_type":"password" }' https://rockstar.auth0.com/oauth/token | jq -r .access_token)
 
-FOLDERS=*/
+FOLDERS=data/*
 for product_folder in $FOLDERS
 do
   echo "uploading $product_folder"
-  RESPONSE_HEADER=$(curl -i -s -X POST $APP_ENDPOINT_URL/products --data @$product_folder/product.json --header "Accept:application/json" --header "Content-Type:application/json; charset=UTF-8" --header "Authorization: Bearer $ACCESS_TOKEN")
+  RESPONSE_HEADER=$(curl -i -s -X POST $API_ENDPOINT_URL/products --data @$product_folder/product.json --header "Accept:application/json" --header "Content-Type:application/json; charset=UTF-8" --header "Authorization: Bearer $ACCESS_TOKEN")
   LOCATION_HEADER=`echo "$RESPONSE_HEADER" | grep Location`
   LOCATION_HEADER=$(echo "$LOCATION_HEADER" | tr -d '\r')
   PRODUCT_HREF=`echo ${LOCATION_HEADER:9}`
